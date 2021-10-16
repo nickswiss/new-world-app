e94acf228a17dee41289637bb52bff8653fabc99
Nick Arnold <nickarnold23@gmail.com>
2021-10-16 12:48:42 -0400

    sweet new updates

--- src/components/Jon.tsx
+++ src/components/Jon.tsx
@@ -1,14 +1,49 @@
 import React from "react";
 import tarkov_sweat from "../images/tarkov_sweat.png";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Jon() {
   return (
     <Container>
-      <div>
+      <Row>
         <h1>Hi Jon!</h1>
+      </Row>
+      <Row>
         <img src={tarkov_sweat} alt="TarkovSweat" />
-      </div>
+      </Row>
+      <Row>
+        <h1>Remember this you asshole!?</h1>
+      </Row>
+      <Row style={{ height: "50vh" }}>
+        <iframe
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          src="https://www.youtube.com/embed/8TlbYm-Enzc"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </Row>
+      <Row>
+        <h1>This was pre-meditated</h1>
+      </Row>
+      <Row style={{ height: "50vh" }}>
+        <iframe
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          src="https://www.youtube.com/embed/Wfcc9SL7-Gg"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </Row>
+
       <Row className="mx-0"></Row>
     </Container>
   );
