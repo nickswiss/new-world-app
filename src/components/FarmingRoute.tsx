247d1dd87d48ea88112535d5334ce51323f064ea
Nick Arnold <nickarnold23@gmail.com>
2021-10-27 15:30:57 -0400

    logging for debug

--- src/components/FarmingRoute.tsx
+++ src/components/FarmingRoute.tsx
@@ -27,6 +27,8 @@ class FarmingRoutes extends React.Component<any, {}> {
       if (!this.props.match.params.id) {
         this.clearActiveRoute();
       } else {
+        console.log(this.props.activeRoute.id);
+        console.log(this.props.match.params.id);
         this.loadRoute();
       }
     }
